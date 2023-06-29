1ce3c5ef9200aaf9d35e47794410b5d1d683d7a5
윤동규 <justinwk@WYBHMacBook-Air.local>
2023-06-29 16:18:13 +0900

    tab screen

---
+++ app/src/main/java/com/example/madcamp_1/BaseActivity.kt
@@ -0,0 +1,78 @@
+package com.example.madcamp_1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.animation.ValueAnimator
+import android.view.MenuItem
+
+
+import kotlinx.android.synthetic.main.activity_splash.*
+
+abstract class BaseActivity : AppCompatActivity() {
+
+    private var instance: BaseActivity? = null
+    private var mToolbarHeight = 0
+    private var mAnimDuration = 0
+
+    private var mVaActionBar: ValueAnimator? = null
+    protected abstract var viewId: Int
+    protected abstract var toolbarId: Int?
+
+    protected abstract fun onCreate()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        instance = this
+
+        setContentView(viewId)
+
+        if (toolbarId != null)
+            findViewById<androidx.appcompat.widget.Toolbar>(toolbarId!!).let {
+                setSupportActionBar(it)
+                supportActionBar?.setDisplayShowTitleEnabled(false)
+            }
+        onCreate()
+    }
+
+    fun hideActionBar() {
+        toolbarId?.let {
+            val mToolbar = findViewById<androidx.appcompat.widget.Toolbar>(toolbarId!!)
+
+            if (mToolbarHeight == 0)
+                mToolbarHeight = mToolbar.height
+
+            if (mVaActionBar != null && mVaActionBar!!.isRunning)
+                return@let
+
+            mVaActionBar = ValueAnimator.ofInt(mToolbarHeight, 0)
+            mVaActionBar?.addUpdateListener {
+                ValueAnimator.AnimatorUpdateListener { animation ->
+                    mToolbar.layoutParams.height = animation.animatedValue as Int
+                    mToolbar.requestLayout()
+                }
+            }
+
+            mVaActionBar?.addUpdateListener {
+                object : AnimatorListenerAdapter() {
+                    override fun onAnimationEnd(animation: Animator?) {
+                        supportActionBar?.hide()
+                    }
+                }
+            }
+
+            mVaActionBar!!.duration = mAnimDuration.toLong()
+            mVaActionBar?.start()
+
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> finish()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+    // <- 버튼 누를 시 뒤로가기
+
+}
--- app/src/main/java/com/example/madcamp_1/MainActivity.kt
+++ app/src/main/java/com/example/madcamp_1/MainActivity.kt
@@ -3,9 +3,58 @@ package com.example.madcamp_1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+class MainActivity : BaseActivity() {
+
+    override var viewId: Int = R.layout.activity_main
+    override var toolbarId: Int? = R.id.toolbar
+    private var mViewPager: ViewPager? = null
+
+    @SuppressLint("ShowToast")
+    override fun onCreate() {
+
+        mViewPager = findViewById(R.id.pager)
+        mViewPager!!.adapter = PagerAdapter(supportFragmentManager)
+        mViewPager!!.currentItem = 0
+
+        val tabLayout = findViewById<View>(R.id.tabs) as TabLayout
+        tabLayout.setupWithViewPager(mViewPager)
+
+
+        tabLayout.getTabAt(0)!!.text = "image 1"
+        tabLayout.getTabAt(1)!!.text = "image 2"
+        tabLayout.getTabAt(2)!!.text = "image 3"
+
+        mViewPager!!.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tabLayout))
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab) = tab.select()
+
+            override fun onTabUnselected(tab: TabLayout.Tab) {
+
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab) {
+
+            }
+        })
+
+    }
+
+    inner class PagerAdapter(supportFragmentManager: FragmentManager) : FragmentStatePagerAdapter(supportFragmentManager) {
+
+        override fun getItem(position: Int): Fragment {
+
+            return when (position) {
+                0 ->
+                    OneFragment()
+                1 ->
+                    TwoFragment()
+                2 ->
+                    ThreeFragment()
+                else ->
+                    TestFragment()
+            }
+        }
+
+        override fun getCount(): Int = 4
     }
 }
