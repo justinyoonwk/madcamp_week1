245acb7a2f4cfc4ce2d117afb3ed103fe2e823b4
윤동규 <justinwk@WYBHMacBook-Air.local>
2023-06-30 16:33:37 +0900

    popup image complete

--- app/src/main/java/com/example/madcamp_1/TwoFragment.kt
+++ app/src/main/java/com/example/madcamp_1/TwoFragment.kt
@@ -8,6 +8,8 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.appcompat.app.AlertDialog
+import android.widget.ImageView
+
 
 
 class TwoFragment : Fragment() {
@@ -48,52 +50,56 @@ class TwoFragment : Fragment() {
         imageAdapter.setImageList(imageList)
         imageAdapter.setOnImageClickListener(object : ImageAdapter.OnImageClickListener {
             override fun onImageClick(imageResId: Int) {
+                val dialogBuilder = AlertDialog.Builder(requireContext())
+                    .setTitle("Image Popup")
+                    .setPositiveButton("OK") { dialog, _ ->
+                        dialog.dismiss()
+                    }
+
+// Inflate custom layout for the dialog
+                val dialogView = layoutInflater.inflate(R.layout.popup_image, null)
+                val imageView = dialogView.findViewById<ImageView>(R.id.imageView)
                 when (imageResId) {
                     R.drawable.image1 -> {
-                        // Show popup dialog specific to image1
-                        val dialogBuilder = AlertDialog.Builder(requireContext())
-                            .setTitle("Image1 Popup")
-                            .setMessage("You clicked on image1")
-                            .setPositiveButton("OK") { dialog, _ ->
-                                dialog.dismiss()
-                            }
-                        dialogBuilder.create().show()
+                        imageView.setImageResource(R.drawable.popup_image1)
                     }
 
                     R.drawable.image2 -> {
-                        // Show popup dialog specific to image2
-                        val dialogBuilder = AlertDialog.Builder(requireContext())
-                            .setTitle("Image2 Popup")
-                            .setMessage("You clicked on image2")
-                            .setPositiveButton("OK") { dialog, _ ->
-                                dialog.dismiss()
-                            }
-                        dialogBuilder.create().show()
+                        imageView.setImageResource(R.drawable.popup_image2)
                     }
 
                     R.drawable.image3 -> {
-                        // Show popup dialog specific to image3
-                        val dialogBuilder = AlertDialog.Builder(requireContext())
-                            .setTitle("Image3 Popup")
-                            .setMessage("You clicked on image3")
-                            .setPositiveButton("OK") { dialog, _ ->
-                                dialog.dismiss()
-                            }
-                        dialogBuilder.create().show()
+                        imageView.setImageResource(R.drawable.popup_image3)
                     }
-                    // Add cases for image4 to image8 here
+
+                    R.drawable.image4 -> {
+                        imageView.setImageResource(R.drawable.popup_image4)
+                    }
+
+                    R.drawable.image5 -> {
+                        imageView.setImageResource(R.drawable.popup_image5)
+                    }
+
+                    R.drawable.image6 -> {
+                        imageView.setImageResource(R.drawable.popup_image6)
+                    }
+
+                    R.drawable.image7 -> {
+                        imageView.setImageResource(R.drawable.popup_image7)
+                    }
+
+                    R.drawable.image8 -> {
+                        imageView.setImageResource(R.drawable.popup_image8)
+                    }
                     else -> {
                         // Handle other images or show a default popup
-                        // Show popup dialog specific to image2
-                        val dialogBuilder = AlertDialog.Builder(requireContext())
-                            .setTitle("default Popup")
-                            .setMessage("You clicked on nothing")
-                            .setPositiveButton("OK") { dialog, _ ->
-                                dialog.dismiss()
-                            }
-                        dialogBuilder.create().show()
+                        // Show popup dialog specific to default
+                        imageView.setImageResource(R.drawable.popup_default)
                     }
                 }
+                dialogBuilder.setView(dialogView)
+
+                dialogBuilder.create().show()
             }
         })
         return view
