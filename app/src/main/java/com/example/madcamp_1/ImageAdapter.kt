22a2abab8e045ec70a4dc15b737c9b9138d15d4e
윤동규 <justinwk@WYBHMacBook-Air.local>
2023-06-30 15:20:12 +0900

    popup

--- app/src/main/java/com/example/madcamp_1/ImageAdapter.kt
+++ app/src/main/java/com/example/madcamp_1/ImageAdapter.kt
@@ -9,11 +9,11 @@ import androidx.recyclerview.widget.RecyclerView
 class ImageAdapter : RecyclerView.Adapter<ImageAdapter.ImageViewHolder>() {
 
     private var imageList: List<Int> = emptyList()
+    private var onImageClickListener: OnImageClickListener? = null
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageViewHolder {
-        val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.item_image, parent, false)
-        return ImageViewHolder(view)
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_image, parent, false)
+        return ImageViewHolder(itemView)
     }
 
     override fun onBindViewHolder(holder: ImageViewHolder, position: Int) {
@@ -30,12 +30,28 @@ class ImageAdapter : RecyclerView.Adapter<ImageAdapter.ImageViewHolder>() {
         notifyDataSetChanged()
     }
 
+    fun setOnImageClickListener(listener: OnImageClickListener) {
+        onImageClickListener = listener
+    }
+
     inner class ImageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
         private val imageView: ImageView = itemView.findViewById(R.id.imageView)
 
         fun bind(imageResId: Int) {
             imageView.setImageResource(imageResId)
-            imageView.scaleType = ImageView.ScaleType.FIT_CENTER // Adjust the scale type here
+
+            itemView.setOnClickListener {
+                val position = adapterPosition
+                if (position != RecyclerView.NO_POSITION) {
+                    val imageResId = imageList[position]
+                    onImageClickListener?.onImageClick(imageResId)
+                }
+            }
         }
+    }
+
+    interface OnImageClickListener {
+        fun onImageClick(imageResId: Int)
     }
 }
--- app/src/main/java/com/example/madcamp_1/TwoFragment.kt
+++ app/src/main/java/com/example/madcamp_1/TwoFragment.kt
@@ -7,7 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.appcompat.app.AlertDialog
 
 
 class TwoFragment : Fragment() {
@@ -25,9 +25,9 @@ class TwoFragment : Fragment() {
         imageRecyclerView = view.findViewById(R.id.imageRecyclerView)
         imageAdapter = ImageAdapter()
 
-        // Set the layout manager with vertical orientation
-        val layoutManager = LinearLayoutManager(context)
-        layoutManager.orientation = LinearLayoutManager.VERTICAL
+        // Set the layout manager with grid layout and 2 columns
+        val layoutManager = GridLayoutManager(context, 2)
+        layoutManager.orientation = GridLayoutManager.VERTICAL
         imageRecyclerView.layoutManager = layoutManager
 
         imageRecyclerView.adapter = imageAdapter
@@ -37,10 +37,67 @@ class TwoFragment : Fragment() {
             R.drawable.image1,
             R.drawable.image2,
             R.drawable.image3,
+            R.drawable.image4,
+            R.drawable.image5,
+            R.drawable.image6,
+            R.drawable.image7,
+            R.drawable.image8,
             // Add more image resource IDs here
         )
-        imageAdapter.setImageList(imageList)
 
+        imageAdapter.setImageList(imageList)
+        imageAdapter.setOnImageClickListener(object : ImageAdapter.OnImageClickListener {
+            override fun onImageClick(imageResId: Int) {
+                when (imageResId) {
+                    R.drawable.image1 -> {
+                        // Show popup dialog specific to image1
+                        val dialogBuilder = AlertDialog.Builder(requireContext())
+                            .setTitle("Image1 Popup")
+                            .setMessage("You clicked on image1")
+                            .setPositiveButton("OK") { dialog, _ ->
+                                dialog.dismiss()
+                            }
+                        dialogBuilder.create().show()
+                    }
+
+                    R.drawable.image2 -> {
+                        // Show popup dialog specific to image2
+                        val dialogBuilder = AlertDialog.Builder(requireContext())
+                            .setTitle("Image2 Popup")
+                            .setMessage("You clicked on image2")
+                            .setPositiveButton("OK") { dialog, _ ->
+                                dialog.dismiss()
+                            }
+                        dialogBuilder.create().show()
+                    }
+
+                    R.drawable.image3 -> {
+                        // Show popup dialog specific to image3
+                        val dialogBuilder = AlertDialog.Builder(requireContext())
+                            .setTitle("Image3 Popup")
+                            .setMessage("You clicked on image3")
+                            .setPositiveButton("OK") { dialog, _ ->
+                                dialog.dismiss()
+                            }
+                        dialogBuilder.create().show()
+                    }
+                    // Add cases for image4 to image8 here
+                    else -> {
+                        // Handle other images or show a default popup
+                        // Show popup dialog specific to image2
+                        val dialogBuilder = AlertDialog.Builder(requireContext())
+                            .setTitle("default Popup")
+                            .setMessage("You clicked on nothing")
+                            .setPositiveButton("OK") { dialog, _ ->
+                                dialog.dismiss()
+                            }
+                        dialogBuilder.create().show()
+                    }
+                }
+            }
+        })
         return view
     }
 }
+
+
