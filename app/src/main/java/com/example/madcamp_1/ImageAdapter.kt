53bd8a359e1faf01e143b793c8a2d5567f209313
MinHee <alsgmlwkd33@sookmyung.ac.kr>
2023-07-03 12:28:01 +0900

    min3

--- app/src/main/java/com/example/madcamp_1/ImageAdapter.kt
+++ app/src/main/java/com/example/madcamp_1/ImageAdapter.kt
@@ -9,7 +9,7 @@ import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.GridLayoutManager
 
 
-class ImageAdapter(private val onItemClick: (Int) -> Unit) : RecyclerView.Adapter<ImageAdapter.ImageViewHolder>() {
+class ImageAdapter : RecyclerView.Adapter<ImageAdapter.ImageViewHolder>() {
 
     private var imageList: List<Int> = emptyList()
     private var onImageClickListener: OnImageClickListener? = null
@@ -40,16 +40,6 @@ class ImageAdapter(private val onItemClick: (Int) -> Unit) : RecyclerView.Adapte
     inner class ImageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
         private val imageView: ImageView = itemView.findViewById(R.id.imageView)
-
-        init {
-            itemView.setOnClickListener {
-                val position = adapterPosition
-                if (position != RecyclerView.NO_POSITION) {
-                    val imageResId = imageList[position]
-                    onItemClick(imageResId)
-                }
-            }
-        }
 
         fun bind(imageResId: Int) {
             adjustImageSize(imageResId)
